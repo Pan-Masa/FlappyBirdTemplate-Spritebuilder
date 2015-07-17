255dd2285c27c7fd5f9b84b46355203af9cad3df
Pan-Masa <nasmac@NasMacs-MacBook-Air.local>
2015-07-18 00:31:52 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,20 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
--(void)update:(CCTime)delta
+- (void)update:(CCTime)delta
 {
     // put update code here
 }
 
 // put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // touches the screen
+    [character flap];
+    
+}
 
 @end
--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -40,7 +40,7 @@
     }
     
     // set this class as delegate
-    character.physicsNode.collisionDelegate = self;
+    physicsNode.collisionDelegate = self;
     
     _obstacles = [NSMutableArray array];
     points = 0;
